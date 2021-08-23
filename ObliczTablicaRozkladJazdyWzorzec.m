36d5bf87a33770b1c9ac450ee71ff2f7154bf22e
marod333 <84393850+marod333@users.noreply.github.com>
2021-08-23 16:08:00 +0200

    Add files via upload

--- ObliczTablicaRozkladJazdyWzorzec.m
+++ ObliczTablicaRozkladJazdyWzorzec.m
@@ -3,22 +3,33 @@
 clear
 clc
 
-global RozkaldJazdyTablica  wymiarTablicyRozkladuJazdy_NaNnaDrogaPrad;
+global RozkaldJazdyTablica  wymiarTablicyRozkladuJazdy_NaNnaDrogaPrad liczbaPojazdow liczbaWpisanychPojazdow;
 %% Utwotrzenie tablicy rozkladu jazdy
 % wymiarRozklaJazdyTablicaNaNnaDrogaPrad = 1 to znaczy jedna kolumna NaN na
 % drogê + jedna kolumna NaN na prad
+
 wymiarTablicyRozkladuJazdy_NaNnaDrogaPrad=5;
 RozkaldJazdyTablica=UtworzRozklaJazdyTablicaNaN(wymiarTablicyRozkladuJazdy_NaNnaDrogaPrad*2);
 
+% Kontrola czy wszystkie pojazdy zosta³y wpisane do RozkaldJazdyTablica
+liczbaPojazdow=0;
+liczbaWpisanychPojazdow=0;
 % Argumenty funkcji
 % NazwaPlikuPrzejazdu_txt, godzynaPrzejazdu, minutaGprzejazdu, sekundaPrzejazdu
 % NazwaPlikuPrzejazdu_txt przyklad: 'przejazd_ED74_W1-250_po_Matlabie_inter.txt', 
-
+RozkaldJazdyTablica=WprowadzRezultatyPrzejazduTeoretycznegoDoRozladuJazdy('przejazd_ED74_W1-250_po_Matlabie_inter.txt', 0 , 0, 2);
 RozkaldJazdyTablica=WprowadzRezultatyPrzejazduTeoretycznegoDoRozladuJazdy('przejazd_ED74_W1-250_po_Matlabie_inter.txt', 0 , 0, 1);
 RozkaldJazdyTablica=WprowadzRezultatyPrzejazduTeoretycznegoDoRozladuJazdy('przejazd_ED74_W1-250_po_Matlabie_inter.txt', 0 , 0, 10);
+
+RozkaldJazdyTablica=WprowadzRezultatyPrzejazduTeoretycznegoDoRozladuJazdy('przejazd_ED74_W1-250_po_Matlabie_inter.txt', 0 , 0, 1);
+RozkaldJazdyTablica=WprowadzRezultatyPrzejazduTeoretycznegoDoRozladuJazdy('przejazd_ED74_W1-250_po_Matlabie_inter.txt', 23 , 59, 58);
+RozkaldJazdyTablica=WprowadzRezultatyPrzejazduTeoretycznegoDoRozladuJazdy('przejazd_ED74_W1-250_po_Matlabie_inter.txt', 23 , 59, 59);
+RozkaldJazdyTablica=WprowadzRezultatyPrzejazduTeoretycznegoDoRozladuJazdy('przejazd_ED74_W1-250_po_Matlabie_inter.txt', 23 , 59, 57);
+
 
 
 % Zamiana NaN na zero
 RozkaldJazdyTablica(isnan(RozkaldJazdyTablica))=0;
 
+clear liczbaPojazdow liczbaWpisanychPojazdow
 save RozkaldJazdyTablica
--- WprowadzRezultatyPrzejazduTeoretycznegoDoRozladuJazdy.m
+++ WprowadzRezultatyPrzejazduTeoretycznegoDoRozladuJazdy.m
@@ -1,6 +1,11 @@
+% Sprawdzone OK
+
 function [RozkaldJazdyTablica] = WprowadzRezultatyPrzejazduTeoretycznegoDoRozladuJazdy(NazwaPlikuPrzejazdu_txt, godzynaPrzejazdu, minutaGprzejazdu, sekundaPrzejazdu)
 
-global RozkaldJazdyTablica wymiarTablicyRozkladuJazdy_NaNnaDrogaPrad;
+global RozkaldJazdyTablica wymiarTablicyRozkladuJazdy_NaNnaDrogaPrad liczbaPojazdow liczbaWpisanychPojazdow;
+% Kontrola czy wszystkie pojazdy zosta³y wpisane do RozkaldJazdyTablica
+liczbaPojazdow=liczbaPojazdow+1;
+
 % Argumenty funkcji 
 % NazwaPlikuPrzejazdu_txt, godzynaPrzejazdu, minutaGprzejazdu, sekundaPrzejazdu
 % NazwaPlikuPrzejazdu_txt przyklad: 'przejazd_ED74_W1-250_po_Matlabie_inter.txt', 
@@ -12,21 +17,80 @@ KomorkaGodzinyPrzejazdu=find(godzynaPrzejazdu==RozkaldJazdyTablica(:,1) & minuta
 %%wpisanie do tablicy rozkladu jazdy przejazdu
 TablicaDrogaPradRezultatPrzejazdu = PobieranieDrogiIPraduZPrzejaduTeoretycznego(NazwaPlikuPrzejazdu_txt);
 dlugoscPrzejazdu=size(TablicaDrogaPradRezultatPrzejazdu,1);
-kontrolaWpisaniaPrzejazduwRozkladJazdy=0;
+% kontrolaWpisaniaPrzejazduwRozkladJazdy=0;
+% kontrolaWpisaniaPrzejazduwRozkladJazdy_przekroczeniePolnocy=0;
+
+liczbaWierszy_RozkaldJazdyTablica=size(RozkaldJazdyTablica,1);
     for dwukrotnoscKolumnyUzupelnianiaNaN = 0:wymiarTablicyRozkladuJazdy_NaNnaDrogaPrad-1 
         % Jeœli z if = 1, wtedy uzupelniaj tablice
-        kontrolaWpisaniaPrzejazduwRozkladJazdy=kontrolaWpisaniaPrzejazduwRozkladJazdy+1;
-        if all(all(isnan(RozkaldJazdyTablica(KomorkaGodzinyPrzejazdu:dlugoscPrzejazdu+KomorkaGodzinyPrzejazdu+1,5+2*dwukrotnoscKolumnyUzupelnianiaNaN:6+2*dwukrotnoscKolumnyUzupelnianiaNaN))))==1
-            RozkaldJazdyTablica(KomorkaGodzinyPrzejazdu,5+2*dwukrotnoscKolumnyUzupelnianiaNaN:6+2*dwukrotnoscKolumnyUzupelnianiaNaN)=0;
-            RozkaldJazdyTablica(KomorkaGodzinyPrzejazdu+1:dlugoscPrzejazdu+KomorkaGodzinyPrzejazdu,5+2*dwukrotnoscKolumnyUzupelnianiaNaN:6+2*dwukrotnoscKolumnyUzupelnianiaNaN)=TablicaDrogaPradRezultatPrzejazdu;
-            RozkaldJazdyTablica(dlugoscPrzejazdu+KomorkaGodzinyPrzejazdu+1,5+2*dwukrotnoscKolumnyUzupelnianiaNaN:6+2*dwukrotnoscKolumnyUzupelnianiaNaN)=0;
-            break
+        if (dlugoscPrzejazdu+KomorkaGodzinyPrzejazdu+1)<=liczbaWierszy_RozkaldJazdyTablica
+%             kontrolaWpisaniaPrzejazduwRozkladJazdy=kontrolaWpisaniaPrzejazduwRozkladJazdy+1;
+            
+            if (all(all(isnan(RozkaldJazdyTablica(KomorkaGodzinyPrzejazdu:dlugoscPrzejazdu+KomorkaGodzinyPrzejazdu+1,5+2*dwukrotnoscKolumnyUzupelnianiaNaN:6+2*dwukrotnoscKolumnyUzupelnianiaNaN)))))==1 
+                
+                RozkaldJazdyTablica(KomorkaGodzinyPrzejazdu,5+2*dwukrotnoscKolumnyUzupelnianiaNaN:6+2*dwukrotnoscKolumnyUzupelnianiaNaN)=0;
+                RozkaldJazdyTablica(KomorkaGodzinyPrzejazdu+1:dlugoscPrzejazdu+KomorkaGodzinyPrzejazdu,5+2*dwukrotnoscKolumnyUzupelnianiaNaN:6+2*dwukrotnoscKolumnyUzupelnianiaNaN)=TablicaDrogaPradRezultatPrzejazdu;
+                RozkaldJazdyTablica(dlugoscPrzejazdu+KomorkaGodzinyPrzejazdu+1,5+2*dwukrotnoscKolumnyUzupelnianiaNaN:6+2*dwukrotnoscKolumnyUzupelnianiaNaN)=0;
+                liczbaWpisanychPojazdow=liczbaWpisanychPojazdow+1;
+                break
+            end
+%           Sprawdzenie czy wymiarTablicyRozkladuJazdy_NaNnaDrogaPrad jest nie
+%           za ma³y, jeœli tak program prosi o powiêkszenie zmiennej: wymiarTablicyRozkladuJazdy_NaNnaDrogaPrad
+%             if dwukrotnoscKolumnyUzupelnianiaNaN==wymiarTablicyRozkladuJazdy_NaNnaDrogaPrad-1 && all(all(isnan(RozkaldJazdyTablica(KomorkaGodzinyPrzejazdu:dlugoscPrzejazdu+KomorkaGodzinyPrzejazdu+1,5+2*dwukrotnoscKolumnyUzupelnianiaNaN:6+2*dwukrotnoscKolumnyUzupelnianiaNaN))))==0                                                                                      
+%                disp('za mala wartosc stalej wymiarTablicyRozkladuJazdy_NaNnaDrogaPrad,')
+%                disp('powieksz ja gdyz przejazd ')
+%                disp(NazwaPlikuPrzejazdu_txt)
+%                disp("nie zostal wpisany w rozklad jazdy");
+%                disp(" ");
+%             end
         end
-        if kontrolaWpisaniaPrzejazduwRozkladJazdy==wymiarTablicyRozkladuJazdy_NaNnaDrogaPrad-1 & all(all(isnan(RozkaldJazdyTablica(KomorkaGodzinyPrzejazdu:dlugoscPrzejazdu+KomorkaGodzinyPrzejazdu+1,5+2*dwukrotnoscKolumnyUzupelnianiaNaN:6+2*dwukrotnoscKolumnyUzupelnianiaNaN))))==0
-           disp('za mala wartosc stalelej wymiarTablicyRozkladuJazdy_NaNnaDrogaPrad,')
-           disp('powieksz ja gdyz przejazd ')
-           disp(NazwaPlikuPrzejazdu_txt)
-           disp("nie zostal wpisany w rozklad jazdy"); 
+        
+        if (dlugoscPrzejazdu+KomorkaGodzinyPrzejazdu+1)>liczbaWierszy_RozkaldJazdyTablica
+           
+%             kontrolaWpisaniaPrzejazduwRozkladJazdy_przekroczeniePolnocy=kontrolaWpisaniaPrzejazduwRozkladJazdy_przekroczeniePolnocy+1;
+%              Sprawdzenie warunku czy mozna wpisac przejazd od polnocy (od 0h,0min,0sek,0dziesietnaSekundy) 
+            if (all(all(isnan(RozkaldJazdyTablica(1:dlugoscPrzejazdu-(liczbaWierszy_RozkaldJazdyTablica-KomorkaGodzinyPrzejazdu+1),5+2*dwukrotnoscKolumnyUzupelnianiaNaN:6+2*dwukrotnoscKolumnyUzupelnianiaNaN)))))==1 ...
+... %          Sprawdzenie warunku czy mozna wpisac przejazd do polnocy  
+               && (all(all(isnan(RozkaldJazdyTablica(KomorkaGodzinyPrzejazdu:liczbaWierszy_RozkaldJazdyTablica,5+2*dwukrotnoscKolumnyUzupelnianiaNaN:6+2*dwukrotnoscKolumnyUzupelnianiaNaN)))))==1
+%              uzupe³niaj przejazd od poczatku rozkladu jazdy Sprawdzone OK
+               RozkaldJazdyTablica(1:dlugoscPrzejazdu-(liczbaWierszy_RozkaldJazdyTablica-KomorkaGodzinyPrzejazdu),5+2*dwukrotnoscKolumnyUzupelnianiaNaN:6+2*dwukrotnoscKolumnyUzupelnianiaNaN)=...
+                                                                         TablicaDrogaPradRezultatPrzejazdu((liczbaWierszy_RozkaldJazdyTablica-KomorkaGodzinyPrzejazdu)+1:dlugoscPrzejazdu,:);
+               RozkaldJazdyTablica(dlugoscPrzejazdu-(liczbaWierszy_RozkaldJazdyTablica-KomorkaGodzinyPrzejazdu)+1,5+2*dwukrotnoscKolumnyUzupelnianiaNaN:6+2*dwukrotnoscKolumnyUzupelnianiaNaN)=0;
+%              uzupelniaj przejazd do polnocy  Sprawdzone OK
+               RozkaldJazdyTablica(KomorkaGodzinyPrzejazdu,5+2*dwukrotnoscKolumnyUzupelnianiaNaN:6+2*dwukrotnoscKolumnyUzupelnianiaNaN)=0;
+               RozkaldJazdyTablica(KomorkaGodzinyPrzejazdu+1:liczbaWierszy_RozkaldJazdyTablica,5+2*dwukrotnoscKolumnyUzupelnianiaNaN:6+2*dwukrotnoscKolumnyUzupelnianiaNaN)=...
+                                                                         TablicaDrogaPradRezultatPrzejazdu(1:(liczbaWierszy_RozkaldJazdyTablica-KomorkaGodzinyPrzejazdu),:);
+               liczbaWpisanychPojazdow=liczbaWpisanychPojazdow+1;
+               break
+                         
+            end
+            
+% %           Sprawdzenie czy wymiarTablicyRozkladuJazdy_NaNnaDrogaPrad jest nie
+% %           za ma³y, jeœli tak program prosi o powiêkszenie zmiennej: wymiarTablicyRozkladuJazdy_NaNnaDrogaPrad
+%             if ... %Sprawdzenie pojazd rozpoczyna swoj przejazd przed polnoca a konczy go po
+%                dwukrotnoscKolumnyUzupelnianiaNaN==wymiarTablicyRozkladuJazdy_NaNnaDrogaPrad-1 &&...
+% ... %          Sprawdzenie warunku czy mozna wpisac przejazd od polnocy (od 0h,0min,0sek,0dziesietnaSekundy)  
+%                (all(all(isnan(RozkaldJazdyTablica(1:dlugoscPrzejazdu-(liczbaWierszy_RozkaldJazdyTablica-KomorkaGodzinyPrzejazdu+1),5+2*dwukrotnoscKolumnyUzupelnianiaNaN:6+2*dwukrotnoscKolumnyUzupelnianiaNaN)))))==0 &&...
+% ... %          Sprawdzenie warunku czy mozna wpisac przejazd do polnocy  
+%                (all(all(isnan(RozkaldJazdyTablica(KomorkaGodzinyPrzejazdu:liczbaWierszy_RozkaldJazdyTablica,5+2*dwukrotnoscKolumnyUzupelnianiaNaN:6+2*dwukrotnoscKolumnyUzupelnianiaNaN)))))==0
+%                    
+%            
+%                disp('za mala wartosc stalej wymiarTablicyRozkladuJazdy_NaNnaDrogaPrad,')
+%                disp('powieksz ja gdyz przejazd ')
+%                disp(NazwaPlikuPrzejazdu_txt)
+%                disp("nie zostal wpisany w rozklad jazdy");
+%                disp("sytuacja dotyczy przejazdu pojazdu, ktory konczy swoj przejazd po pó³nocy");
+%                disp(" ");
+%             end  
         end
+    end
+    
+    if liczbaWpisanychPojazdow<liczbaPojazdow
+       disp('za mala wartosc stalej wymiarTablicyRozkladuJazdy_NaNnaDrogaPrad,')
+       disp('powieksz ja, gdyz przejazd ')
+       disp([NazwaPlikuPrzejazdu_txt, ' godzynaPrzejazdu= ', num2str(godzynaPrzejazdu), ', minutaPrzejazdu= ', num2str(minutaGprzejazdu), ', sekundaPrzejazdu= ', num2str(sekundaPrzejazdu)])
+       disp("nie zostal wpisany w rozklad jazdy")
+       disp(" ");
+       liczbaWpisanychPojazdow=liczbaWpisanychPojazdow+1;
     end
 end
